dabe91bcd1ab14f6df6ecf5ae78a334aba10c1aa
srikanth <srikanth.kunaa2@gmail.com>
2024-02-07 12:26:26 +0530

    change4

--- aks.sh
+++ aks.sh
@@ -10,7 +10,6 @@ sudo apt-get install -y azure-cli
 az login
 
 # Set the subscription
-az account set --subscription "f7f1ad4b-cddb-4191-8a24-4b50fada8af9"
 
 # Create a resource group
 az group create --name myResourceGroup --location East US
@@ -18,7 +17,8 @@ az group create --name myResourceGroup --location East US
 # Create AKS cluster with multiple node pools
 az aks create --resource-group myResourceGroup --name myAKSCluster --enable-managed-identity --node-count 2 --generate-ssh-keys
 
-sudo snap install kubectl
+sudo snap install kubectl --classic
+
 kubectl version --short
 
 kubectl get nodes
