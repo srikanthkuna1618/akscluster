7d3bf451fa987f92aef810c0c55746c1727cb226
srikanth <srikanth.kunaa2@gmail.com>
2024-02-07 12:23:47 +0530

    change3

--- aks.sh
+++ aks.sh
@@ -5,17 +5,6 @@ sudo apt-get update
 sudo apt-get install -y azure-cli
 
 # Variables
-RESOURCE_GROUP_NAME="myResourcegroup"
-CLUSTER_NAME="myakscluster"
-LOCATION="East US"
-NODE_POOL1_NAME="nodepool1"
-NODE_POOL2_NAME="nodepool2"
-NODE_POOL1_SIZE="Standard_DS2_v2"   # Sample node size, change as needed
-NODE_POOL2_SIZE="Standard_DS3_v2"   # Sample node size, change as needed
-NODE_POOL1_MIN_NODES=1
-NODE_POOL1_MAX_NODES=2
-NODE_POOL2_MIN_NODES=1
-NODE_POOL2_MAX_NODES=2
 
 # Log in to Azure
 az login
@@ -24,22 +13,12 @@ az login
 az account set --subscription "f7f1ad4b-cddb-4191-8a24-4b50fada8af9"
 
 # Create a resource group
-az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
+az group create --name myResourceGroup --location East US
 
 # Create AKS cluster with multiple node pools
-az aks create \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --name $CLUSTER_NAME \
-    --node-count 3 \
-    --enable-addons monitoring \
-    --generate-ssh-keys \
-    --nodepool-name $NODE_POOL1_NAME \
-    --node-vm-size $NODE_POOL1_SIZE \
-    --node-count $NODE_POOL1_MIN_NODES \
-    --min-count $NODE_POOL1_MIN_NODES \
-    --max-count $NODE_POOL1_MAX_NODES \
-    --nodepool-name $NODE_POOL2_NAME \
-    --node-vm-size $NODE_POOL2_SIZE \
-    --node-count $NODE_POOL2_MIN_NODES \
-    --min-count $NODE_POOL2_MIN_NODES \
-    --max-count $NODE_POOL2_MAX_NODES
+az aks create --resource-group myResourceGroup --name myAKSCluster --enable-managed-identity --node-count 2 --generate-ssh-keys
+
+sudo snap install kubectl
+kubectl version --short
+
+kubectl get nodes
